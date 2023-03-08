0e23afd011d8db129435b87cda7db66777091a2c
maria <maria.ic.mihai@gmail.com>
2023-03-08 22:53:18 +0100

    Integrate with the db

--- cmd/main.go
+++ cmd/main.go
@@ -4,16 +4,27 @@ import (
 	"fmt"
 	"hexagonal/internal/adaptors/app/api"
 	"hexagonal/internal/adaptors/core/arithmetic"
+	"hexagonal/internal/adaptors/framework/driven/db"
 	"hexagonal/internal/ports"
+	"log"
 )
 
 func main() {
-	// ports
+	// core
 	var core ports.ArithmeticPort
 	core = arithmetic.NewAdapter()
 
+	// framework layer
+	// DB
+	var dbAdapter ports.DBPort
+	dbAdapter, err := db.NewAdapter("driverName", "dataSourceName")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// application layer
 	var app ports.APIPort
-	app = api.NewAdapter(core)
+	app = api.NewAdapter(core, dbAdapter)
 
 	result, err := app.GetAddition(1, 1)
 	if err != nil {
--- internal/adaptors/app/api/api.go
+++ internal/adaptors/app/api/api.go
@@ -3,51 +3,61 @@ package api
 import "hexagonal/internal/ports"
 
 type Adapter struct {
-	arithPort ports.ArithmeticPort
+	arithmeticPort ports.ArithmeticPort
+	dbPort         ports.DBPort
 }
 
-func NewAdapter(arithPort ports.ArithmeticPort) *Adapter {
+func NewAdapter(arithPort ports.ArithmeticPort, dbPort ports.DBPort) *Adapter {
 	return &Adapter{
-		arithPort: arithPort,
+		arithmeticPort: arithPort,
+		dbPort:         dbPort,
 	}
 }
 
-func (adapt Adapter) GetAddition(a, b int) (int, error) {
-	answer, err := adapt.arithPort.Addition(a, b)
+func (apiAdapter Adapter) GetAddition(a, b int) (int, error) {
+	answer, err := apiAdapter.arithmeticPort.Addition(a, b)
 
 	if err != nil {
 		return 0, err
 	}
+
+	err = apiAdapter.dbPort.AddToHistory(answer, "addition")
 
 	return answer, nil
 }
 
-func (adapt Adapter) GetSubtraction(a, b int) (int, error) {
-	answer, err := adapt.arithPort.Subtraction(a, b)
+func (apiAdapter Adapter) GetSubtraction(a, b int) (int, error) {
+	answer, err := apiAdapter.arithmeticPort.Subtraction(a, b)
 
 	if err != nil {
 		return 0, err
 	}
+
+	err = apiAdapter.dbPort.AddToHistory(answer, "subtraction")
 
 	return answer, nil
 }
 
-func (adapt Adapter) GetMultiplication(a, b int) (int, error) {
-	answer, err := adapt.arithPort.Multiplication(a, b)
+func (apiAdapter Adapter) GetMultiplication(a, b int) (int, error) {
+	answer, err := apiAdapter.arithmeticPort.Multiplication(a, b)
 
 	if err != nil {
 		return 0, err
 	}
+
+	err = apiAdapter.dbPort.AddToHistory(answer, "multiplication")
 
 	return answer, nil
 }
 
-func (adapt Adapter) GetDivision(a, b int) (int, error) {
-	answer, err := adapt.arithPort.Division(a, b)
+func (apiAdapter Adapter) GetDivision(a, b int) (int, error) {
+	answer, err := apiAdapter.arithmeticPort.Division(a, b)
 
 	if err != nil {
 		return 0, err
 	}
+
+	err = apiAdapter.dbPort.AddToHistory(answer, "division")
 
 	return answer, nil
 }
